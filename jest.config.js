a8b094d1af18ab98cc044e1e3f485200f083e164
Alexander Sychev <shurik.shurik.1993@yandex.ru>
2020-08-06 03:13:49 +0300

    Fix timeout error

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ const path = require('path');
 module.exports = {
   bail: true,
   testEnvironment: 'node',
-  rootDir: path.join(__dirname, 'dist')
+  rootDir: path.join(__dirname, 'dist'),
+  testTimeout: 1000 * 60 * 5, // 5 minutes test timeout
 };
